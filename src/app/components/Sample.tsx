d2a3a5e5709392309ef2de0b8b0ccd24ad650e06
Uwancha <sito2alemu@gmail.com>
2023-11-19 11:31:52 +0300

    Initial commit

---
+++ src/app/components/Sample.tsx
@@ -0,0 +1,12 @@
+
+    import React from 'react';
+
+const SampleComponent: React.FC = () => {
+  return (
+    <div>
+      <h1>Hello, Next.js with Firestore!</h1>
+    </div>
+  );
+};
+
+export default SampleComponent;
