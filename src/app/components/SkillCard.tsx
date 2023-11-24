c3c5128a6cc08800f9bfa452b73c373d2e601c9f
Uwancha <sito2alemu@gmail.com>
2023-11-24 16:00:12 +0300

    feat: Create SkillCard component to display each skill in a card

---
+++ src/app/components/SkillCard.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Link from "next/link";
+
+const SkillCard = ({skill}) => {
+    return (
+        <div className="flex flex-col gap-4 p-4 rounded-lg shadow-lg">
+            <h4 className="text-slategray font-bold">{skill.name}</h4>
+            <p className="text-slategray">{skill.description}</p>
+            <Link href={`/skills/${skill.id}`} className='link'>See Full Details</Link>
+        </div>
+    );
+};
+
+export default SkillCard;
