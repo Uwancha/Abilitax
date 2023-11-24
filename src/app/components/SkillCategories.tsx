3784e03fd1fa688757436c7487be280332c200f6
Uwancha <sito2alemu@gmail.com>
2023-11-24 15:56:33 +0300

    refactor: Refactor SkillCategories component Refactor the logic of data fetching Make the code maintainable

--- src/app/components/SkillCategories.tsx
+++ src/app/components/SkillCategories.tsx
@@ -3,6 +3,7 @@
 import React, { useEffect, useState } from 'react';
 import { collection, getDocs} from 'firebase/firestore';
 import { db } from '../firebaseConfig';
+import SkillCard from './SkillCard';
 
 interface Skill {
   categories: any;
@@ -21,21 +22,15 @@ const SkillsCategories: React.FC = () => {
       try {
         const docRef = collection(db, 'skills');
         const docs = await getDocs(docRef);
-
+        console.log(docs)
         const skills: Skill[]  = [];
 
         docs.forEach((doc) => {
           skills.push({
-            name: doc.id,
+            id: doc.id,
             ...doc.data(),
-            data: [],
-            description: '',
-            resources: '',
-            categories: undefined
           });
         });
-
-        console.log(skills)
 
         if (skills.length) {
           setData(skills)
@@ -54,9 +49,8 @@ const SkillsCategories: React.FC = () => {
   return (
     <div>
       <div>{data?.map(d => (
-          <article key={d.name}>
-            <h1>{d.name}</h1>
-            <p>{d.description}</p>
+          <article className='bg-white flex flex-col gap-1 mb-5 mx-auto w-1/2 ' key={d.name}>
+            <SkillCard skill={d} />
           </article>
       ) ) } 
       </div>
