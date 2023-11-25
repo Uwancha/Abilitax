170fe9b5322aa9e84b283d7ef7e5cbaf43f45838
Uwancha <sito2alemu@gmail.com>
2023-11-25 20:39:58 +0300

    refactor: Refactor SkillDetails Map over additional data and display Adjust alignment of some elements

--- src/app/components/SkillDetails.tsx
+++ src/app/components/SkillDetails.tsx
@@ -33,20 +33,21 @@ export default function SkillDetails({id}) {
     let courseElements;
     let books;
     let ytChannels;
+    let additionalElements
 
     if(details?.resources) {
       resourceElements = details.resources.map(resource => (
         <div key={resource.name} className='my-5'>
-          <h3 className='text-center mb-3 font-bold'>{resource.name}</h3>
-          <p>{resource.about}</p>
-          <Link href={resource.website} className='link'>{resource.name}</Link>
+          <h3 className='text-slategray text-2xl font-bold mb-5'>{resource.name}</h3>
+          <p className='pl-10' >{resource.about}</p>
+          <Link href={resource.website} className='link pl-10' >{resource.name}</Link>
         </div>
       ));
     }
 
     if(details?.courses) {
       courseElements = details.courses.map(course => (
-        <div key={course.name} className='pt-10'>
+        <div key={course.name} className='pl-10 my-5'>
           <h3 className='mb-3  font-bold'>{course.name}</h3>
           <p>{course.about}</p>
           <Link href={course.website} className='link'>{course.name}</Link>
@@ -73,6 +74,16 @@ export default function SkillDetails({id}) {
         </div>
       ));
     }
+
+    if(details?.others) {
+      additionalElements = details.others.map(other => (
+        <div key={other.name} className='pl-10 my-5 '>
+          <h3 className='mb-3  font-bold'>{other.name}</h3>
+          <p>{other.about}</p>
+          <Link href={other.website} className='link'>{other.name}</Link>
+        </div>
+      ));
+    }
   
   return (
       <div className='w-2/4 mx-auto mt-20 py-10 px-5 bg-white rounded-lg shadow-lg'>
@@ -80,9 +91,9 @@ export default function SkillDetails({id}) {
         <p className='text-slategray'>{details?.description}</p>
       
         <section className='my-10 text-slategray'>
-          <p>
+          <h5 className='text-slategray font-bold' >
             Here I list what you need to kick start your journey into the world of {details?.name}
-          </p>
+          </h5>
           <div>{resourceElements}</div>
           <div className='my-10' >
             <h2 className='text-slategray text-2xl font-bold' >Professional Certificates</h2>
@@ -95,6 +106,11 @@ export default function SkillDetails({id}) {
           <div className='my-10' >
             <h2  className='text-slategray text-2xl font-bold mb-5 ' >YouTube Channels</h2>
             {ytChannels}
+          </div>
+
+          <div className='my-10' >
+            <h2  className='text-slategray text-2xl font-bold mb-5 ' >Additional Resources</h2>
+            {additionalElements}
           </div>
         </section>
       </div>
