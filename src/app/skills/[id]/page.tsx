0de7601c138bebcc8b09e13a14c3de8dd695beb7
Uwancha <sito2alemu@gmail.com>
2023-11-28 22:58:23 +0300

    design: Import and display Footer inside Skills and SkillDetails

--- src/app/skills/[id]/page.tsx
+++ src/app/skills/[id]/page.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 import Nav from "@/app/components/Header";
 import SkillDetails from "@/app/components/SkillDetails";
+import Footer from "@/app/components/Footer";
 
 export default function SkillDetailsPage({params}) {
   const id = params.id;
 
   return (
     <main>
-      <div className='skill-details pt-10 pb-10'>
+      <div className='skill-details pt-10 '>
         <Nav />
         <SkillDetails id={id} />
+        <Footer />
       </div>
     </main>
   )
--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Nav from '../components/Header';
 import SkillsCategories from '../components/SkillCategories';
+import Footer from '../components/Footer';
 
 const Skills: React.FC = () => {
     return (
         <div className='skills pt-10'>
             <Nav />
            <SkillsCategories /> 
+           <Footer />
         </div> 
     )
 }
