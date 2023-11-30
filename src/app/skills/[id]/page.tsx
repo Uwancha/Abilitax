b2489db46e8f0c1591eca05a123c8b6aab771e6f
Uwancha <sito2alemu@gmail.com>
2023-11-30 11:20:53 +0300

    refactor: Wrap SkillDetails around Layout

--- src/app/skills/[id]/page.tsx
+++ src/app/skills/[id]/page.tsx
@@ -2,16 +2,18 @@ import React from "react";
 import Nav from "@/app/components/Header";
 import SkillDetails from "@/app/components/SkillDetails";
 import Footer from "@/app/components/Footer";
+import Layout from "@/app/components/layoutSkillDetails";
 
 export default function SkillDetailsPage({params}) {
   const id = params.id;
 
   return (
     <main>
+      
       <div className='skill-details pt-10 '>
-        <Nav />
-        <SkillDetails id={id} />
-        <Footer />
+        <Layout metaTitle={'gjdgjfgkjfgkj'}>
+          <SkillDetails id={id} />
+        </Layout>
       </div>
     </main>
   )
