2edf1d74077faeb648256da1a612265b21560e67
Uwancha <sito2alemu@gmail.com>
2023-12-02 23:49:46 +0300

    ui: Add hover effect to SkilldCard

--- src/app/components/SkillCard.tsx
+++ src/app/components/SkillCard.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const SkillCard = ({skill}) => {
     return (
-        <div className="flex flex-col gap-4 p-4 rounded-lg shadow-lg">
+        <div className="flex flex-col gap-4 p-4 rounded-lg shadow-lg hover:shadow">
             <h4 className="text-slategray font-bold">{skill.name}</h4>
             <p className="text-slategray">{skill.description}</p>
             <Link href={`/skills/${skill.id}`} className='link'>See Full Details</Link>
