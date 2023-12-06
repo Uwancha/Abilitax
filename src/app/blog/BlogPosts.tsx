f43679fa9236a77d3ba6854c5f46ee23ed8c1d3e
Uwancha <sito2alemu@gmail.com>
2023-12-06 15:46:50 +0300

    feat: Create BlogPosts component This commit creates a BlogPosts componen to list all blog posts The component fetches data and passes it to children to display individual post cards

---
+++ src/app/blog/BlogPosts.tsx
@@ -0,0 +1,58 @@
+'use client'
+
+import React, { useEffect, useState } from 'react';
+import { collection, getDocs} from 'firebase/firestore';
+import { db } from '../firebaseConfig';
+
+// Components
+import BlogCard from './BlogCard';
+
+interface Blog {
+  title: string
+  data: []
+  blog: string
+  description: string
+}
+
+
+const BlogPosts: React.FC = () => {
+  const [posts, setPosts] = useState<Blog[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const docRef = collection(db, 'blogs');
+        const docs = await getDocs(docRef);
+        console.log(docs)
+        const blogs: Blog[]  = [];
+
+        docs.forEach((doc) => blogs.push({
+            id: doc.id,
+            ...doc.data(),
+        }));
+
+        if (blogs.length) {
+          setPosts(blogs)
+        } else {
+          console.log('No such document!');
+        }
+      } catch (error) {
+        console.error('Error fetching document:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+  console.log(posts);
+
+  return (
+    <div className='mb-10'>
+      <div className="posts">{posts?.map(post => (
+            <BlogCard post = {post} key={post.id} />
+      ) ) } 
+      </div>
+    </div>
+  );
+};
+
+export default BlogPosts;
