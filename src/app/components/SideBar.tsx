0f3349ed63b1834cea555026dce4ccbe7b7dd964
Uwancha <sito2alemu@gmail.com>
2023-12-06 11:00:20 +0300

    feat: Create SideBar for Blog This commit creates a SidBar for the Blog page with placeholder contents

---
+++ src/app/components/SideBar.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export const SideBar: React.FC = () => {
+    return (
+        <article>
+            <h5>Front-End Development</h5>
+            <h5>Freelancing</h5>
+            <h5>Back-End Development</h5>
+            <h5>Social Media Marketing </h5>
+            <h5>Content Creating</h5>
+            <h5>UX/UI Design</h5>
+            <h5>Mobile App Development</h5>
+            <h5>Cloud Computing</h5>
+            <h5>Graphic Design</h5>
+            <h5>Video Editing</h5>
+            <h5>Self Development</h5>
+            <h5>Other</h5>
+        </article>
+    )
+}
