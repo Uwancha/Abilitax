a144b12d0dd76538a5e6427e2327d22b1464651d
Uwancha <sito2alemu@gmail.com>
2023-12-10 23:27:08 +0300

    refactor: Refactor SkillDetails component Improve code organization, modularize rendering functions, and enhance readability.

--- src/app/components/SkillDetails.tsx
+++ src/app/components/SkillDetails.tsx
@@ -5,119 +5,105 @@ import { db } from '@/app/firebaseConfig'
 import {doc, onSnapshot } from 'firebase/firestore'
 import Link from 'next/link';
 
-function getSkillDetails(id) {
+async function getSkillDetails(id) {
   const docref = doc(db, 'skills', id);
 
-  return new Promise((resolve) => {
-    onSnapshot(docref, (doc) => {
-      resolve(doc.data());
-    });
-  });
+  try {
+    const snapshot = await onSnapshot(docref)
+      return snapshot.data();
+  
+  } catch (error) {
+    console.error('Error fetching skill details:', error);
+    return null
+  }
+}
+
+const RenderResource = ({ resource }) => {
+  return (
+    <div key={resource.name} className='my-5 pl-10'>
+      <h5 className='text-xl font-light mb-5'>{resource.name}</h5>
+      <p className='pl-10 mb-5'>{resource.about}</p>
+      <Link href={resource.website || resource.link} className='link'>
+        View Resource
+      </Link>
+    </div>
+  );
+}
+
+const SkillSection = ({ title, elements }) => {
+  return (
+    <div className='my-10'>
+      <h5 className='text-blackish font-bold pl-10 text-xl'>{title}</h5>
+      {elements}
+    </div>
+  );
 }
 
 export default function SkillDetails({id}) {
     const [details, setSkillDetails] = useState<[]>()
-    
+    const [loading, setLoading] = useState<boolean>(false)
 
     useEffect(() => {
       async function getData() {
+        try {
         const data = await getSkillDetails(id);  
         setSkillDetails(data);
+        } catch {
+          setLoading(true)
+        }
       }
   
       getData();
   
     }, [id])
-   
-    let resourceElements;
-    let courseElements;
-    let books;
-    let ytChannels;
-    let additionalElements
 
-    if(details?.resources) {
-      resourceElements = details.resources.map(resource => (
-        <div key={resource.name} className='my-5 pl-10'>
-          <h5 className='text-xl font-light mb-5'>{resource.name}</h5>
-          <p className='pl-10' >{resource.about}</p>
-          <Link href={resource.website} className='link' >View Resource</Link>
-        </div>
-      ));
-    }
-
-    if(details?.courses) {
-      courseElements = details.courses.map(course => (
-        <div key={course.name} className='pl-10 my-5'>
-          <h5 className='mb-3  font-light'>{course.name}</h5>
-          <p>{course.about}</p>
-          <Link href={course.website} className='link'>View Resource</Link>
-        </div>
-      ));
-    }
-
-    if(details?.books) {
-      books = details.books.map(book => (
-        <div key={book.name} className='pl-10 my-5'>
-          <h5 className='mb-3  font-light'>{book.name}</h5>
-          <p>{book.about}</p>
-          <Link href={book.link} className='link'>Link to Book</Link>
-        </div>
-      ));
-    }
-
-    if(details?.youtube) {
-      ytChannels = details.youtube.map(yt => (
-        <div key={yt.name} className='pl-10 my-5'>
-          <h5 className='mb-3  font-light'>{yt.name}</h5>
-          <p>{yt.about}</p>
-          <Link href={yt.link} className='link'>Link to Channel</Link>
-        </div>
-      ));
-    }
-
-    if(details?.others) {
-      additionalElements = details.others.map(other => (
-        <div key={other.name} className='pl-10 my-5 '>
-          <h5 className='mb-3  font-light'>{other.name}</h5>
-          <p>{other.about}</p>
-          <Link href={other.website} className='link'>View Resource</Link>
-        </div>
-      ));
+    if (loading) {
+      return <p>Loading...</p>;
     }
   
+
+    const {books, conclusion, conclusionmore, conclusionlast, courses, resources, name, youtube, others} = details
+    
+    const resourceElements = resources.map(resource => (
+      <RenderResource resource={resource} /> 
+    ) );
+    const courseElements = courses.map(course => ( 
+      <RenderResource resource={course} />
+    ));
+    const booksElements = books.map(book => ( 
+      <RenderResource resource={book} />
+    ));
+    const ytChannels = youtube.map(yt => ( 
+      <RenderResource resource={yt} />
+    ));
+    
+    const additionalElements = others.map(other => (
+      <RenderResource resource={other} />
+    ));
+  
   return (
-      <div className='w-2/4 mx-auto py-20 px-5 bg-white '>
+      <div className='w-2/4 mx-auto py-10 px-5 mt-10 mb-10 bg-white rounded-lg '>
         <h2 className='text-blackish text-2xl text-center font-bold mb-5 '>{details?.name}</h2>
       
         <section className='my-10 text-blackish'>
           <h3 className='text-blackish text-xl font-bold mb-10' >
             All you need to kick start your journey into the world of {details?.name}
           </h3>
+
           <div>{resourceElements}</div>
-          {details?.courses && <div className='my-10' >
-            <h5 className='text-blackish font-bold pl-10 text-xl ' >Professional Certificates</h5>
-            {courseElements}
-          </div> }
-          {details?.books && <div className='my-10' >
-            <h5  className='text-blackish text-xl pl-10 font-bold mb-5 ' >Books</h5>
-            {books}
-          </div> }
-          {details?.youtube && <div className='my-10' >
-            <h5  className='text-blackish text-xl pl-10 font-bold mb-5 ' >YouTube Channels</h5>
-            {ytChannels}
-          </div> }
+          {courses && <SkillSection title='Professional Certificates' elements={courseElements} />}
+          {books && <SkillSection title='Books' elements={booksElements} />}
+          {youtube && <SkillSection title='YouTube Channels' elements={ytChannels} /> }
+          {others && <SkillSection title='Additional Resources' elements={additionalElements} />}
 
-          {details?.others &&<div className='my-10' >
-            <h5  className='text-blackish text-xl font-bold pl-10 mb-5 ' >Additional Resources</h5>
-            {additionalElements}
-          </div> }
-
-          {details?.conclusion && <div>
-            <h5 className='text-blackish text-xl font-bold pl-10 mb-5 '>Conclusion</h5>
-            <p className='text-blackish font-light pl-10 mb-3'>{details?.conclusion}</p>
-            <p className='text-blackish font-light pl-10  mb-3'>{details?.conclusionmore}</p>
-            <p className='text-blackish font-light pl-10'>{details?.conclusionlast}</p>
-          </div> }
+          {conclusion && (
+            <div>
+              <h5 className='text-blackish text-xl font-bold pl-10 mb-5 '>Conclusion</h5>
+              <p className='text-blackish font-light pl-10 mb-3'>{conclusion}</p>
+              <p className='text-blackish font-light pl-10  mb-3'>{conclusionmore}</p>
+              <p className='text-blackish font-light pl-10'>{conclusionlast}</p>
+            </div> 
+          )}
         </section>
       </div>
   )
