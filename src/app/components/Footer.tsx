302244d4bd75a1093da554c937803fb94796e45b
Uwancha <sito2alemu@gmail.com>
2023-12-10 22:22:51 +0300

    refactor: Refactor Footer Add logo image and edit the description of the platform. Remove unnecessary links. Add urls to links. Change color, adjust alignments and improve the ui

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -4,67 +4,74 @@ import Link from "next/link";
 
 import tikokIcon from "../../../public/tiktok.svg";
 import linkedinIcon from "../../../public/linkedin-original.svg";
-import twitterIcon from "../../../public/twitter_3670151.png";
 import instagramIcon from "../../../public/instagram.svg";
+import telegramIcon from "../../../public/telegram.png";
 
 const Footer: React.FC = () => {
 
     return (
-        <footer className="footer flex justify-center gap-20 py-10">
+        <footer className="footer text-blackish flex justify-center gap-20 py-10">
                 <div >
-                    <h2 className="mb-2 text-xl font-bold">MerekVerse</h2>
-                    <p>MerekVerse provides curated resources <br />
-                     for those who want to learn in demand skills</p>
+                    <Link href='/' className="flex items-center justify-start mb-6 ml-auto hover:opacity-50" >
+                        <Image src={'/logo-.png'} alt="logo"  width={100} height={150} />
+                        <h2 className="abilitax mb-2 text-silver text-3xl font-bold" style={{marginLeft:'-20px'}}>Abilitax</h2>
+                    </Link>
+                    <h5 className="font-light">AbiliTax curate only the highest quality curriculums, tutorials, courses <br /> 
+                    and guides from across the web and organize them taxonomically.</h5>
                 </div>
 
                 <section className="footer-icons flex flex-col">
-                    <h4 className="mb-5 text-xl  font-bold">Social Links</h4>
-                    <div className="flex justify-between items-center mb-4">
-                        <p>LinkedIn</p>
+                    <h4 className="mb-5 text-xl  font-light">Social Links</h4>
+                    <Link href={'https://www.linkedin.com/in/sitota'} className="flex justify-between items-center mb-4 hover:opacity-50">
+                        <p className="font-light" >LinkedIn</p>
                         <Image 
                             src={linkedinIcon}
                             width={20} 
                             alt={"link to LinkedIn"}
+                            className=""
                         />
-                    </div>
-                    <div className="flex justify-between items-center mb-4">
-                        <p>TikTok</p>
+                    </Link>
+                    <Link href={'https://www.tiktok.com/@uwancha_?_t=8i4EEQfkwa6&_r=1'} className="flex justify-between items-center mb-4 hover:opacity-50">
+                        <p className="font-light" >TikTok</p>
                         <Image 
                             src={tikokIcon} 
                             width={20} 
                             alt={"link to TikTok"} 
+                            className=""
                         />
-                    </div>
-                    <div className="flex justify-between items-center mb-4">
-                        <p>Twitter</p>
-                        <Image 
-                            src={twitterIcon} 
-                            width={20} 
-                            alt={"link to Twitter"} 
-                        />
-                    </div>
-                    <div className="flex justify-between items-center gap-4 mb-4">
-                        <p>Instagram</p>
+                    </Link>
+                    <Link href={'https://instagram.com/sitota_a?igshid=MzMyNGUyNmU2YQ=='} className="flex justify-between items-center gap-4 mb-4 hover:opacity-50">
+                        <p className="font-light" >Instagram</p>
                         <Image 
                             src={instagramIcon} 
                             width={20} 
                             alt={"link to instagram"}
+                            className=""
                         />
-                    </div>
+                    </Link>
+                    <Link href={'https://t.me/abilitax'} className="flex justify-between items-center gap-4 mb-4 hover:opacity-50">
+                        <p className="font-light" >Telegram</p>
+                        <Image 
+                            src={telegramIcon} 
+                            width={20} 
+                            alt={"link to instagram"}
+                            className=""
+                        />
+                    </Link>
+
                 </section>
             
                 <div className="flex flex-col">
-                    <h4 className="mb-5 text-xl font-bold">About Us</h4>
-                    <Link href="" className="mb-3">About</Link>
-                    <Link href="" className="mb-3">Blog</Link>
-                    <Link href="" className="mb-3">Contact</Link>
+                    <h4 className="mb-5 text-xl font-light">About Us</h4>
+                    <Link href="/about" className="mb-3 font-light hover:opacity-50">About</Link>
+                    <Link href="/blog" className="mb-3 font-light hover:opacity-50">Blog</Link>
+                    <Link href="/about#contact" className="mb-3 font-light hover:opacity-50">Contact</Link>
                 </div>
                 
                 <div className="flex flex-col">
-                    <h4 className="mb-5 text-xl font-bold">Guides</h4>
-                    <Link href="" className="mb-3">Mentorship</Link>
-                    <Link href="" className="mb-3">YouTube Channel</Link>
-                    <Link href="" className="mb-3">Blog</Link>
+                    <h4 className="mb-5 text-xl font-light">Guides</h4>
+                    <Link href="https://www.youtube.com/channel/UCdPLGussDH7auaCYrc05M3Q" className="mb-3 font-light hover:opacity-50">YouTube Channel</Link>
+                    <Link href="/blog" className="mb-3 font-light hover:opacity-50">Blog</Link>
                 </div>  
         </footer>
 
