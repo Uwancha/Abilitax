9b84ac9acccbf199eca739ee2383662d6fc8af71
Uwancha <sito2alemu@gmail.com>
2023-12-11 01:09:07 +0300

    fix: Fix getSkillDetails function Replace onSnapshot with getDoc for proper Promise handling and add check for document existence.

--- src/app/components/SkillDetails.tsx
+++ src/app/components/SkillDetails.tsx
@@ -2,15 +2,21 @@
 
 import React, { useEffect, useState } from 'react'
 import { db } from '@/app/firebaseConfig'
-import {doc, onSnapshot } from 'firebase/firestore'
+import { doc, getDoc } from 'firebase/firestore'
 import Link from 'next/link';
 
 async function getSkillDetails(id) {
   const docref = doc(db, 'skills', id);
 
   try {
-    const snapshot = await onSnapshot(docref)
-      return snapshot.data();
+    const docSnapshot = await getDoc(docref)
+    if (docSnapshot.exists()) {
+      console.log(docSnapshot.data())
+      return docSnapshot.data();
+    } else {
+      console.error('Skill does not exist');
+      return null;
+    }
   
   } catch (error) {
     console.error('Error fetching skill details:', error);
@@ -40,14 +46,14 @@ const SkillSection = ({ title, elements }) => {
 }
 
 export default function SkillDetails({id}) {
-    const [details, setSkillDetails] = useState<[]>()
+    const [details, setSkillDetails] = useState<{}>()
     const [loading, setLoading] = useState<boolean>(false)
 
     useEffect(() => {
       async function getData() {
         try {
-        const data = await getSkillDetails(id);  
-        setSkillDetails(data);
+          const data = await getSkillDetails(id);
+          setSkillDetails(data);
         } catch {
           setLoading(true)
         }
@@ -60,48 +66,45 @@ export default function SkillDetails({id}) {
     if (loading) {
       return <p>Loading...</p>;
     }
-  
-
-    const {books, conclusion, conclusionmore, conclusionlast, courses, resources, name, youtube, others} = details
     
-    const resourceElements = resources.map(resource => (
+    const resourceElements = details?.resources.map(resource => (
       <RenderResource resource={resource} /> 
     ) );
-    const courseElements = courses.map(course => ( 
+    const courseElements = details?.courses.map(course => ( 
       <RenderResource resource={course} />
     ));
-    const booksElements = books.map(book => ( 
+    const booksElements = details?.books.map(book => ( 
       <RenderResource resource={book} />
     ));
-    const ytChannels = youtube.map(yt => ( 
+    const ytChannels = details?.youtube.map(yt => ( 
       <RenderResource resource={yt} />
     ));
     
-    const additionalElements = others.map(other => (
+    const additionalElements = details?.others.map(other => (
       <RenderResource resource={other} />
-    ));
+    )); 
   
   return (
       <div className='w-2/4 mx-auto py-10 px-5 mt-10 mb-10 bg-white rounded-lg '>
-        <h2 className='text-blackish text-2xl text-center font-bold mb-5 '>{details?.name}</h2>
+        <h2 className='text-blackish text-2xl text-center font-bold mb-5 '>{name}</h2>
       
         <section className='my-10 text-blackish'>
           <h3 className='text-blackish text-xl font-bold mb-10' >
-            All you need to kick start your journey into the world of {details?.name}
+            All you need to kick start your journey into the world of {name}
           </h3>
 
           <div>{resourceElements}</div>
-          {courses && <SkillSection title='Professional Certificates' elements={courseElements} />}
-          {books && <SkillSection title='Books' elements={booksElements} />}
-          {youtube && <SkillSection title='YouTube Channels' elements={ytChannels} /> }
-          {others && <SkillSection title='Additional Resources' elements={additionalElements} />}
+          {details?.courses && <SkillSection title='Professional Certificates' elements={courseElements} />}
+          {details?.books && <SkillSection title='Books' elements={booksElements} />}
+          {details?.youtube && <SkillSection title='YouTube Channels' elements={ytChannels} /> }
+          {details?.others && <SkillSection title='Additional Resources' elements={additionalElements} />}
 
-          {conclusion && (
+          {details?.conclusion && (
             <div>
               <h5 className='text-blackish text-xl font-bold pl-10 mb-5 '>Conclusion</h5>
-              <p className='text-blackish font-light pl-10 mb-3'>{conclusion}</p>
-              <p className='text-blackish font-light pl-10  mb-3'>{conclusionmore}</p>
-              <p className='text-blackish font-light pl-10'>{conclusionlast}</p>
+              <p className='text-blackish font-light pl-10 mb-3'>{details?.conclusion}</p>
+              <p className='text-blackish font-light pl-10  mb-3'>{details?.conclusionmore}</p>
+              <p className='text-blackish font-light pl-10'>{details?.conclusionlast}</p>
             </div> 
           )}
         </section>
