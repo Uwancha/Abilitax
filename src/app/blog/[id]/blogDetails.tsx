c911c14f63f17b01327fbeac465da67268c38530
Uwancha <sito2alemu@gmail.com>
2023-12-11 01:08:23 +0300

    fix: Fix getBlogDetails function Replace onSnapshot with getDoc for proper Promise handling and add check for document existence.

--- src/app/blog/[id]/blogDetails.tsx
+++ src/app/blog/[id]/blogDetails.tsx
@@ -2,7 +2,7 @@
 
 import React, { useEffect, useState } from 'react'
 import { db } from '@/app/firebaseConfig'
-import {doc, onSnapshot } from 'firebase/firestore'
+import {doc, getDoc } from 'firebase/firestore'
 import Link from 'next/link';
 import Layout from './layout';
 
@@ -13,8 +13,14 @@ async function getBlogDetails(id) {
   const docref = doc(db, 'blogs', id);
 
   try {
-    const onSnapshot = await onSnapshot(docref)
-      return onSnapshot.data();
+    const docSnapshot = await getDoc(docref)
+    
+    if (docSnapshot.exists()) {
+      return docSnapshot.data();
+    } else {
+      console.error('Blog does not exist');
+      return null;
+    }
     
   } catch (error) {
     console.log('An error has occured', error);
@@ -64,7 +70,7 @@ export default function Details({id}) {
         try {
           const data = await getBlogDetails(id);  
           setBlogDetails(data);
-        } finally {
+        } catch {
           setLoading(true);
         }
       }
@@ -75,68 +81,65 @@ export default function Details({id}) {
 
     if (loading) return (<p>Loading...</p>)
 
-    const { author, authorImage, createdat, content,conclusion,
-      image, introduction, summary, title, tip, readmin
-    } = details;
+    let formatedDate;
     
-    
-    const dateFormat = 'MMM dd, yyyy'
-    const formatedDate = format(createdat.toDate(), dateFormat)
+    if (details?.createdat) {
+      const dateFormat = 'MMM dd, yyyy'
+      formatedDate = format(details?.createdat.toDate(), dateFormat)
+    }
 
-
-    
-    const introductionElements = introduction.map(intro => (
+    const introductionElements = details?.introduction.map(intro => (
         <RenderIntroduction intro={intro} />
       ));
     
 
     
-    const blogBody = content.map(c => (
+    const blogBody = details?.content.map(c => (
         <RenderContent content={c} />
       ));
     
     const  tipElement =  (
-      <RenderTip tip={tip} />
+      <RenderTip tip={details?.tip} />
     );
     
     const conclusionElement = (
-      <RenderConclusion conclusion={conclusion}  />
+      <RenderConclusion conclusion={details?.conclusion}  />
     );
   
   
   return (
     <Layout
         title={'AbiliTax-Guide to become Full-Stack developer'}
-        description={summary}
+        description={details?.summary}
     >
       <img
-        src={image} 
-        alt={title} 
+        src={details?.image} 
+        alt={details?.title} 
         className='w-2/4 mt-10 mx-auto '
       />
       <div className='w-2/4 mx-auto py-10 px-5 bg-white rounded-lg text-xl '>
       <div className="blogCard-title mb-8" >
-          <img src={authorImage} className="authorImage" alt="Author's image" />
+          <img src={details?.authorImage} className="authorImage" alt="Author's image" />
           <div className="author-date" >
-              <p className='text-blacklish font-bold mb-.5' >{author}</p>
+              <p className='text-blacklish font-bold mb-.5' >{details?.author}</p>
               <p className='text-blacklish' >{formatedDate}</p>
           </div>
       </div>
-        <h3 className='text-blacklish text-2xl text-center font-bold mb-5 '>{title}</h3>
+        <h3 className='text-blacklish text-2xl text-center font-bold mb-5 '>{details?.title}</h3>
       
         <section className='my-10 text-blacklish'>
           <div>
             <h5 className='mb-3 pl-10 font-bold'>Introduction</h5>
             {introductionElements}
           </div>
-          {content && <div className='my-10' >
+          {details?.content && <div className='my-10' >
             {blogBody}
           </div> }
-          {conclusion && <div className='my-10' >
+          {details?.conclusion && <div className='my-10' >
             <h5  className='text-blacklish text-xl font-bold pl-10 mb-5 ' >Conclusion</h5>
             {conclusionElement}
           </div> }
-          {tip && <div className='mx-10 bg-whitesmoke text-slategray rounded-lg my-10' >
+          {details?.tip && <div className='mx-10 bg-whitesmoke text-slategray rounded-lg my-10' >
             {tipElement}
           </div> }
 
