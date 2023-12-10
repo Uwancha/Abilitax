42df684e831cc033a8dc6e07c6a4fe3d3a24a17c
Uwancha <sito2alemu@gmail.com>
2023-12-10 22:24:59 +0300

    refactor/ui: Refactor Header Add logo image and effects to navigation links

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,18 +3,22 @@
 import React, { useState, useEffect } from "react";
 
 import Link from "next/link";
+import Image from "next/image";
 
 
 const Nav: React.FC = ({sty}) => {
-
+    
     return (
         <div className="flex justify-between py-10" style={sty}>
-            <h3 className="header-h3 text-2xl font-bold">Merek Verse</h3>
+            <Link href='/' className="flex items-center hover:opacity-50">
+                <Image src={'/logo-.png'} alt='logo' width={100} height={150} />
+                <h3 className="text-silver text-3xl font-bold" style={{marginLeft:'-20px'}}>Abilitax</h3>
+            </Link>
             <nav className="flex">
-                <Link href="/" className="text font-bold mr-3">Home</Link>
-                <Link href="/skills" className="text font-bold mr-3">Skills</Link>
-                <Link href="/blog" className="text font-bold mr-3">Blog</Link> 
-                <Link href="/about" className="text font-bold mr-3">About</Link>  
+                <Link href="/" className="text font-bold mr-3 hover:opacity-50">Home</Link>
+                <Link href="/skills" className="text font-bold mr-3 hover:opacity-50">Skills</Link>
+                <Link href="/blog" className="text font-bold mr-3 hover:opacity-50">Blog</Link> 
+                <Link href="/about" className="text font-bold mr-3 hover:opacity-50">About</Link>  
             </nav>
         </div>
     )
