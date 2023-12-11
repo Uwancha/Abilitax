4d5d49185b6f9033c325808fa5c1e59a26176fa8
Uwancha <sito2alemu@gmail.com>
2023-12-11 20:45:38 +0300

    feat: Create LatestBlogCard Create a card to diplay latest/featured blog posts. This component takes props from LatestBlogPosts

---
+++ src/app/components/latestBlogPostCard.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Link from 'next/link';
+
+
+const LatestBlogCard = ({post}) => {
+
+  return (
+    <section
+    className="w-full mx-auto p-6
+    rounded-lg bg-white text-blackish
+    shadow-md" >
+
+      <h2 className='text-blacklish text-xl font-bold mb-2'>{post.title}</h2>
+      <p className='text-blacklish font-light my-6'>{post.summary.split(0, 50)}</p>
+      <div className="cta mt-4">
+        <p className='text-blacklish'>{post.readmin}</p>
+        <Link href={`blog/${post.id}`} 
+          className='hover:text-slategray'
+        >
+          Read Article
+        </Link>
+      </div>
+    </section>
+  )
+}
+
+export default LatestBlogCard;
