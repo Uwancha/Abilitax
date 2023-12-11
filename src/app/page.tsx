3d64ff9df21504a84256ac5fd3734366206b0a70
Uwancha <sito2alemu@gmail.com>
2023-12-11 20:57:03 +0300

    refactor: Refactor Home Modularize and structure code for better readability and maintenance Improve the design and ui Improve the content. Improve the hero section's content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,157 +1,105 @@
 import Image from "next/image";
 import Link from "next/link";
 
-//Image assets
-import rabbitHoleImage from "../../public//man-1204925_640.jpg";
-import tutorailHellImage from "../../public/man-7412527_640.png"
-import willAIImage from "../../public/artificial-intelligence-3382507_640.jpg";
-import aiImage from "../../public/artificial-intelligence-6767502_640.jpg";
-import pickSkillsImage from "../../public/cartoon-3082809_640.png";
-import projectIdeasImage from "../../public/website-3374825_640.jpg"
+// Layout
 import Layout from "./components/layoutHome";
+
+// components
+import LatestBlogPosts from "./components/latestBlogPosts";
 
 
 export default function Home() {
   return (
     <main>
-    <Layout metaTitle={'dfkndkfjkdfdf'} >
+    <Layout metaTitle={'Abilitax'} >
       <div className="home">
-            <article className="hero">
+            <header className="hero">
                 <h1 className="text-3xl font-extrabold mt-60 pl-60">
-                 Collections of curated resources!
+                    Collections of curated resources!
                 </h1>
 
                 <article className="article-about text-xl mt-10 pl-60">
-                    <h2>
-                        Often learning new skill is not frustrating but picking the right resources!
-                    </h2>
-                    <h2 className="mt-4">
-                        We've collections best curaated resources to kick start your learning journey so you don't have to waste time searching them!
-                    </h2>
-
+                    <p>
+                        Often learning new skills is frustrating due to picking the right resources.
+                    </p>
+                    <p className="mt-4">
+                        We've curated the best resources to kickstart your learning journey efficiently.
+                    </p>
                     <Link
-                        href="/skills"
-                        className="w-3/4 mr-auto text-center text-white mt-20 py-4
-                        text-xl bg-green border border-green-626 rounded-full shadow-md 
-                        hover:bg-white hover:text-slategray 
-                        hover:border hover:border-green-626
-                        transition duration-500 ease-in-out"
-                    >
-                        Explore
-                    </Link>
+                    href="/skills"
+                    className="w-2/4 text-center text-white mt-20 mb-10 py-4
+                    text-xl  border border-green-626 rounded-full shadow-md 
+                    hover:bg-white hover:text-slategray 
+                    hover:border hover:border-blackish
+                    transition duration-1000 ease-in-out"
+                >
+                    Browse Resources
+                </Link>
                 </article>
-            </article>
+                <Link href={'#talents'}
+                    className="text-white text-5xl mx-auto mt-auto pb-4 hover:bg-transparent
+                    hover:opacity-60 transition duration-500 ease-in-out"
+                >&darr;</Link>
+            </header>
 
-            <article className="article-talents">
-                <h3 className="text-slategray font-bold text-2xl text-center">Most Sought-After Talents</h3>
+            <article id="talents" className="article-talents">
+                <h3 className="text-blackish font-bold text-2xl text-center">Most Sought-After Talents</h3>
                 
                 <div className="talents">
-                    <section className="skill-card py-5 hover:shadow" > 
-                        <h4 className="text-slategray font-bold text-xl pl-5">Software Engineering</h4>
-                        <p className="text-slategray pl-5">A brief introduction about the skill...</p>
-                        <Link href="" className="link pl-5">Learn More</Link>
+                    <section className="skill-card text-blackish pb-5 hover:shadow hover:scale-105 transition duration-500 ease-in-out" > 
+                        <Image src={'/web-development-.png'} className="w-full " width={100} height={100} alt="" />
+                        <h4 className="font-bold text-xl pl-5 mt-5 mb-2">Web Development</h4>
+                        <p className="font-light pl-5 mb-5">Web development involves building and maintaining websites. Developers are responsible for the front-end (visual design) and back-end (functionality) aspects using technologies like HTML, CSS, JavaScript, PHP, Python, etc.</p>
+                        <Link href="/skills/qiLrR0J8w9H0N5l3q7um" className="link pl-5 mt-auto">Learn More</Link>
                     </section>
-                    <section className="skill-card py-5 hover:shadow" >
-                        <h4 className="text-slategray font-bold text-xl pl-5">Data Science</h4>
-                        <p className="text-slategray pl-5">A brief introduction about the skill...</p>
-                        <Link href="" className="link pl-5">Learn More</Link>
+                    <section className="skill-card text-blackish pb-5 hover:shadow hover:scale-105 transition duration-500 ease-in-out" >
+                    <Image src={'/Data-Science-.png'} className="w-full " width={100} height={100} alt="" />
+                        <h4 className="font-bold text-xl pl-5">Data Science</h4>
+                        <p className="font-light pl-5">Data scientists analyze structured and unstructured data to discover hidden patterns, build predictive models and gain useful insights.</p>
+                        <Link href="/skills/aG8wdlTi0BCyOi0iGWar" className="link pl-5 mt-auto">Learn More</Link>
                     </section>
-                    <section className="skill-card py-5 hover:shadow" >
-                        <h4 className="text-slategray font-bold text-xl pl-5">Cloud Computing</h4>
-                        <p className="text-slategray pl-5">A brief introduction about the skill...</p>
-                        <Link href="" className="link pl-5">Learn More</Link>
+                    <section className="skill-card text-blackish pb-5 hover:shadow hover:scale-105 transition duration-500 ease-in-out" >
+                    <Image src={'/cloud-computing-.png'} className="w-full " width={100} height={100} alt="" />
+                        <h4 className="font-bold text-xl pl-5">Cloud Computing</h4>
+                        <p className="font-light pl-5">Cloud computing allows users to access technology-enabled services like storage, databases, analytics, networking, software, etc. Cloud professionals apply technical, engineering and architectural skills to deploy, manage, secure and optimize cloud computing systems.</p>
+                        <Link href="/skills/BVwJPrtgDEuZA1i3Nwdl" className="link pl-5 mt-auto">Learn More</Link>
                     </section>
-                    <section className="skill-card py-5 hover:shadow" >
-                        <h4 className="text-slategray font-bold text-xl pl-5">UI/UX Design</h4>
-                        <p className="text-slategray pl-5">A brief introduction about the skill...</p>
-                        <Link href="" className="link pl-5">Learn More</Link>
+                    <section className="skill-card text-blackish pb-5 hover:shadow hover:scale-105 transition duration-500 ease-in-out" >
+                    <Image src={'/UX-UI-Design.png'} className="w-full " width={100} height={100} alt="" />
+                        <h4 className="font-bold text-xl pl-5">UI/UX Design</h4>
+                        <p className="font-light pl-5">UI/UX designers plan and build intuitive, attractive user experiences across digital platforms. They apply principles of human-computer interaction and usability.</p>
+                        <Link href="/skills/gAuIqxghNT2xeB3jA5he" className="link pl-5 mt-auto">Learn More</Link>
                     </section>
-                    <section className="skill-card py-5 hover:shadow" >
-                        <h4 className="text-slategray font-bold text-xl pl-5">Video Editing</h4>
-                        <p className="text-slategray pl-5">A brief introduction about the skill...</p>
-                        <Link href="" className="link pl-5">Learn More</Link>
+                    <section className="skill-card text-blackish pb-5 hover:shadow hover:scale-105 transition duration-500 ease-in-out" >
+                    <Image src={'/video-editing.jpg'} className="w-full " width={100} height={100} alt="" />
+                        <h4 className="font-bold text-xl pl-5">Video Editing</h4>
+                        <p className="font-light pl-5">Video editors cut and assemble media like footage, audio, graphics and visual effects to craft compelling stories or messages for various platforms.</p>
+                        <Link href="/skills/i4XqhRH4B6hPNhPqKD7N" className="link pl-5 mt-auto">Learn More</Link>
                     </section>
-                    <section className="skill-card py-5 hover:shadow" >
-                        <h4 className="text-slategray font-bold text-xl pl-5">Digital Marketing</h4>
-                        <p className="text-slategray pl-5">A brief introduction about the skill...</p>
-                        <Link href="" className="link pl-5">Learn More</Link>
+                    <section className="skill-card text-blackish pb-5 hover:shadow hover:scale-105 transition duration-500 ease-in-out" >
+                    <Image src={'/Content-marketing.png'} className="w-full " width={100} height={100} alt="" />
+                        <h4 className="font-bold text-xl pl-5">Content Creation</h4>
+                        <p className="font-light pl-5">Content creation is the process of researching, writing, editing, and publishing various types of content for digital platforms like websites, blogs, social media, etc.</p>
+                        <Link href="/skills/EGDWNVURqnXoGn8Axut3" className="link pl-5 mt-auto">Learn More</Link>
                     </section>
                 </div>
+
+                <Link
+                    href="/skills"
+                    className="w-1/4 mx-auto text-center text-white mt-10 mb-10 py-4
+                    text-xl bg-blackish border border-green-626 rounded-full shadow-md 
+                    hover:bg-white hover:text-slategray 
+                    hover:border hover:border-blackish
+                    transition duration-500 ease-in-out"
+                >
+                    Explore More
+                </Link>
 
             </article>
 
-            <article className="article-blog">
-                <h4 className="text-slategray text-center font-bold text-2xl" >Some of the Best Tips for Your Journey</h4>
-                
-                <div className="featured-blogs">
-                    <section className="blog-card py-5" >
-                        <h4 className="text-slategray font-bold text-xl" >What is a rabbit hole and how to escape it</h4>
-                        <Image 
-                          src={rabbitHoleImage}
-                          width={40} alt={""}
-                          quality={100}
-                          className="blog-image"
-                        />
-                        <p className="text-slategray pl-3">A brief introduction about the topic...</p>
-                        <Link href={''} className="link mr-auto pl-3" >Continue reading...</Link>  
-                    </section>
-                    <section className="blog-card py-5" >
-                        <h4 className="text-slategray font-bold text-xl " >What is a tutorial hell</h4>
-                        <Image 
-                          src={tutorailHellImage}
-                          width={40} alt={""}
-                          quality={100}
-                          className="blog-image"
-                        />
-                        <p className="text-slategray pl-3">A brief introduction about the topic...</p>
-                        <Link href={''} className="link mr-auto pl-3" >Continue reading...</Link>
-                    </section>
-                    <section className="blog-card py-5" >
-                        <h4 className="text-slategray font-bold text-xl " >Which skill Should I pick?</h4>
-                        <Image 
-                          src={pickSkillsImage}
-                          width={40} alt={""}
-                          quality={100}
-                          className="blog-image"
-                        />
-                        <p className="text-slategray pl-3">A brief introduction about the topic...</p>
-                        <Link href={''} className="link mr-auto pl-3" >Continue reading...</Link>
-                    </section>
-                    <section className="blog-card py-5" >
-                        <h4 className="text-slategray font-bold text-xl " >How to use AI as an aspiring Developer</h4>
-                        <Image 
-                          src={aiImage}
-                          width={40} alt={""}
-                          quality={100}
-                          className="blog-image"
-                        />
-                        <p className="text-slategray pl-3">A brief introduction about the topic...</p>
-                        <Link href={''} className="link mr-auto pl-3" >Continue reading...</Link>
-                    </section>
-                    <section className="blog-card py-5">
-                        <h4 className="text-slategray font-bold text-xl "  >Will AI replace programmers?</h4>
-                        <Image 
-                          src={willAIImage}
-                          width={40} alt={""}
-                          quality={100}
-                          className="blog-image"
-                        />
-                        <p className="text-slategray pl-3">A brief introduction about the topic...</p>
-                        <Link href={''} className="link mr-auto pl-3" >Continue reading...</Link>
-                    </section>
-                    <section className="blog-card py-5">
-                        <h4 className="text-slategray font-bold text-xl" >5 best Project ideas for front-end developer</h4>
-                        <Image 
-                          src={projectIdeasImage}
-                          width={40} alt={""}
-                          quality={100}
-                          className="blog-image"
-                        />
-                        <p className="text-slategray pl-3">A brief introduction about the topic...</p>
-                        <Link href={''} className="link mr-auto pl-3" >Continue reading...</Link>
-                    </section>
-                </div>
-
+            <article className="featured-blogs py-20">
+                <h4 className="text-blackish text-center font-bold text-2xl mb-20" >Featured Blog Posts</h4>
+                <LatestBlogPosts />
             </article>
         </div>
         </Layout>
