a738ddc6f66fb56ad1c9afb5dce639813012fe09
Uwancha <sito2alemu@gmail.com>
2023-12-12 23:50:31 +0300

    feat: Create loading files

---
+++ src/app/blog/loading.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Loading() {
+    return (
+        <div className="loading">
+            <button className="circular-btn"></button>
+            <button className="circular-btn"></button>
+            <button className="circular-btn"></button>
+        </div>
+    )
+}
+
+
