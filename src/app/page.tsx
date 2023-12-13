b6d97a3581471bfff3ff18e64617b22ec89dd997
Uwancha <sito2alemu@gmail.com>
2023-12-13 22:41:51 +0300

    ui: Optimize image quality in Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -59,37 +59,37 @@ export default function Home() {
                 
                 <div className='talents'>
                     <section className='skill-card text-blackish pb-5 hover:shadow hover:scale-105 transition duration-500 ease-in-out' > 
-                        <Image src={'/web-development-.png'} className='w-full ' width={100} height={100} alt='' />
+                        <Image src={'/web-development-.png'} className='w-full ' width={100} height={100} alt='' quality={100} />
                         <h4 className='font-bold text-xl pl-5 mt-5 mb-2'>Web Development</h4>
                         <p className='font-light pl-5 mb-5'>Web development involves building and maintaining websites. Developers are responsible for the front-end (visual design) and back-end (functionality) aspects using technologies like HTML, CSS, JavaScript, PHP, Python, etc.</p>
                         <Link href='/skills/qiLrR0J8w9H0N5l3q7um' className='link pl-5 mt-auto'>Learn More</Link>
                     </section>
                     <section className='skill-card text-blackish pb-5 hover:shadow hover:scale-105 transition duration-500 ease-in-out' >
-                    <Image src={'/Data-Science-.png'} className='w-full ' width={100} height={100} alt='' />
+                    <Image src={'/Data-Science-.png'} className='w-full ' width={100} height={100} alt='' quality={100} />
                         <h4 className='font-bold text-xl pl-5'>Data Science</h4>
                         <p className='font-light pl-5'>Data scientists analyze structured and unstructured data to discover hidden patterns, build predictive models and gain useful insights.</p>
                         <Link href='/skills/aG8wdlTi0BCyOi0iGWar' className='link pl-5 mt-auto'>Learn More</Link>
                     </section>
                     <section className='skill-card text-blackish pb-5 hover:shadow hover:scale-105 transition duration-500 ease-in-out' >
-                    <Image src={'/cloud-computing-.png'} className='w-full ' width={100} height={100} alt='' />
+                    <Image src={'/cloud-computing-.png'} className='w-full ' width={100} height={100} alt='' quality={100} />
                         <h4 className='font-bold text-xl pl-5'>Cloud Computing</h4>
                         <p className='font-light pl-5'>Cloud computing allows users to access technology-enabled services like storage, databases, analytics, networking, software, etc. Cloud professionals apply technical, engineering and architectural skills to deploy, manage, secure and optimize cloud computing systems.</p>
                         <Link href='/skills/BVwJPrtgDEuZA1i3Nwdl' className='link pl-5 mt-auto'>Learn More</Link>
                     </section>
                     <section className='skill-card text-blackish pb-5 hover:shadow hover:scale-105 transition duration-500 ease-in-out' >
-                    <Image src={'/UX-UI-Design.png'} className='w-full ' width={100} height={100} alt='' />
+                    <Image src={'/UX-UI-Design.png'} className='w-full ' width={100} height={100} alt='' quality={100} />
                         <h4 className='font-bold text-xl pl-5'>UI/UX Design</h4>
                         <p className='font-light pl-5'>UI/UX designers plan and build intuitive, attractive user experiences across digital platforms. They apply principles of human-computer interaction and usability.</p>
                         <Link href='/skills/gAuIqxghNT2xeB3jA5he' className='link pl-5 mt-auto'>Learn More</Link>
                     </section>
                     <section className='skill-card text-blackish pb-5 hover:shadow hover:scale-105 transition duration-500 ease-in-out' >
-                    <Image src={'/video-editing.jpg'} className='w-full ' width={100} height={100} alt='' />
+                    <Image src={'/video-editing.jpg'} className='w-full ' width={100} height={100} alt='' quality={100} />
                         <h4 className='font-bold text-xl pl-5'>Video Editing</h4>
                         <p className='font-light pl-5'>Video editors cut and assemble media like footage, audio, graphics and visual effects to craft compelling stories or messages for various platforms.</p>
                         <Link href='/skills/i4XqhRH4B6hPNhPqKD7N' className='link pl-5 mt-auto'>Learn More</Link>
                     </section>
                     <section className='skill-card text-blackish pb-5 hover:shadow hover:scale-105 transition duration-500 ease-in-out' >
-                    <Image src={'/Content-marketing.png'} className='w-full ' width={100} height={100} alt='' />
+                    <Image src={'/Content-marketing.png'} className='w-full ' width={100} height={100} alt='' quality={100} />
                         <h4 className='font-bold text-xl pl-5'>Content Creation</h4>
                         <p className='font-light pl-5'>Content creation is the process of researching, writing, editing, and publishing various types of content for digital platforms like websites, blogs, social media, etc.</p>
                         <Link href='/skills/EGDWNVURqnXoGn8Axut3' className='link pl-5 mt-auto'>Learn More</Link>
