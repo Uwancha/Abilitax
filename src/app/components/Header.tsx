a8fef27ecc1e3007b942cd281c3c98b5445743d4
Uwancha <sito2alemu@gmail.com>
2023-12-15 00:23:39 +0300

    feat: Add responsive mobile nav menu Conditionally render DesktopMenu or MobileMenu Toggle mobile menu open/close with button close menu on outside click Import humburger and close icons Refactor Nav component

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,25 +1,86 @@
 'use client'
 
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
+
+import { useMediaQuery } from "react-responsive";
+import { FaBars, FaTimes } from 'react-icons/fa'
 
 import Link from "next/link";
 import Image from "next/image";
 
 
+const breakPoint = 768
+
+const DesktopMenu = () => {
+    return (
+        <nav className="flex">
+            <Link href="/" className="  mr-3 hover:opacity-50 sm:font-bold">Home</Link>
+            <Link href="/skills" className=" mr-3 hover:opacity-50 sm:font-bold">Skills</Link>
+            <Link href="/blog" className=" mr-3 hover:opacity-50 sm:font-bold">Blog</Link> 
+            <Link href="/about" className=" mr-3 hover:opacity-50 sm:font-bold">About</Link>  
+        </nav> 
+    )
+}
+
+const MobileMenu = ({ isopen, mobile, toggleMenu } : {isopen : boolean, mobile: boolean, toggleMenu : any}) => {
+    return ( 
+        <div>
+        {mobile && <FaBars 
+            className="cursor-pointer mr-4"
+            onClick={toggleMenu}
+            size={30}
+        /> }
+        {isopen && <div className="fixed top-0 left-0 w-full h-full bg-black/50" onClick={toggleMenu}>
+        <div className="absolute top-0 left-0 w-full h-full bg-black/70 px-20 text-silver" >
+            <NavLinks close={toggleMenu} />
+          
+            <FaTimes
+                className="absolute right-1 top-20 mr-4"
+                onClick={toggleMenu}
+                size={30}
+            /> 
+        </div>
+      </div> }
+      </div> 
+    );
+  };
+  
+  const NavLinks = ({ close } : {close : any}) => (
+    <nav className="flex flex-col items-start gap-10 mt-60">
+      <Link href="/" onClick={close} className="text-2xl hover:opacity-50">
+        Home
+      </Link>
+      <Link href="/skills" onClick={close} className="text-2xl hover:opacity-50">
+        Skills
+      </Link>
+      <Link href="/blog" onClick={close} className="text-2xl hover:opacity-50">
+        Blog
+      </Link>
+      <Link href="/about" onClick={close} className="text-2xl hover:opacity-50">
+        About
+      </Link>
+    </nav>
+  );
+
+
 const Nav: React.FC = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const isMobile = useMediaQuery({ maxWidth: breakPoint });
+
+    const toggleMenu = () => setIsOpen(!isOpen);
     
     return (
-        <div className="flex justify-between items-center py-1 border sm:border-none sm:px-20 text-slategray position-fixed top-0">
+        <div className="flex justify-between items-center py-1 sm:px-20 text-slategray position-fixed top-0">
             <Link href='/' className="flex items-center hover:opacity-50">
                 <Image src={'/logo-.png'} alt='logo' className="w-[80px] sm:w-[150px]" width={100} height={150} priority />
                 <h3 className="text-silver  font-bold sm:text-3xl" style={{marginLeft:'-15px'}}>Abilitax</h3>
             </Link>
-            <nav className="flex">
-                <Link href="/" className="text  mr-3 hover:opacity-50 sm:font-bold">Home</Link>
-                <Link href="/skills" className="text mr-3 hover:opacity-50 sm:font-bold">Skills</Link>
-                <Link href="/blog" className="text mr-3 hover:opacity-50 sm:font-bold">Blog</Link> 
-                <Link href="/about" className="text mr-3 hover:opacity-50 sm:font-bold">About</Link>  
-            </nav>
+
+            {isMobile ? (
+                <MobileMenu mobile={isMobile} isopen={isOpen} toggleMenu={toggleMenu} />
+            ) : ( 
+                <DesktopMenu />  
+            ) }
         </div>
     )
 }
