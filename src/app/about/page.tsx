b92078ef34a1c0af43e114715c8607cf42611b07
Uwancha <sito2alemu@gmail.com>
2024-05-01 03:26:15 +0300

    feat/ui: Iterate the UI design of About page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,39 +7,29 @@ import Layout from './layout';
 import Nav from '../components/Header';
 import Footer from '../components/Footer';
 
-// Creators photo
-import creatorsPhoto from './1666710204004.jpeg'
-
-const About: React.FC = () => {
-    
+const About = () => {
     return (
         <Layout >
             <Nav />
-            <div className='about-home w-full'>
-                <section className='about sm:w-3/5 mx-auto flex flex-col gap-16 mt-10 pb-20 px-4' >
-                    <h1 className='text-blackish text-center text-4xl font-bold mt-4' >About AbiliTax</h1>
-                    <p className='sm:text-xl text-blackish font-light' >
-                        AbiliTax is an online skills development platform addressing the growing
-                        demand for training in web development, design, marketing and other careers
-                        in our country.
+            <div className='w-full bg-default'>
+                <section className='midnight sm:w-3/5 mx-auto flex flex-col gap-8 mt-10 pb-20 px-4' >
+                    <h1 className='text-center text-3xl mt-4'>About Abilitax</h1>
+                    <p className='sm:text-xl font-light'>
+                        Abilitax is a platform where people get starter packs for their first or next carrer in varios discplines such web development, design, marketing and others.
                     </p>
-                    <p className='sm:text-xl text-blackish font-light'>
+                    <p className='sm:text-xl font-light'>
                         With Internet resources scattered and often overwhelming for beginners,
                         our goal is to simplify the skills learning journey. AbiliTax curate only the
                         highest quality tutorials, courses and guides from across the web and
                         organize them taxonomically based on topic.
                     </p>
-                    <p className='sm:text-xl text-blackish font-light' >
+                    <p className='sm:text-xl font-light' >
                         In addition to structured content library, AbiliTax produces original learning
                         resources through our blog and YouTube channel. Here you ll find helpful tips,
                         guides, and video explanations to supplement your skills growth.
                     </p>
-                    <p className='sm:text-xl text-blackish font-light' >
-                        By bringing together resources from across the Internet and creating new
-                        content tailored primarily for our local market, AbiliTax aims to become
-                        the premier hub for anyone seeking accessible skills training in our country.
-                    </p>
-                    <p className='sm:text-xl text-blackish font-light' >
+            
+                    <p className='sm:text-xl font-light' >
                         Start expanding your abilities today by exploring our content or subscribing
                         to our YouTube channel for updates. We look forward to supporting you on
                         your learning journey!
@@ -48,18 +38,18 @@ const About: React.FC = () => {
 
             </div>
 
-            <div className='contact border border-t flex flex-col-reverse px-5 sm:px-0 sm:items-center sm:justify-center
+            <div className='bg-blackish border border-t flex flex-col-reverse px-5 sm:px-0 sm:items-center sm:justify-center
              sm:gap-40 sm:flex-row py-40'
             >
                 <div>
                     <Image
-                        src={creatorsPhoto} alt={'creator photo'} 
+                        src={'/1666710204004.jpeg'} alt={'creator photo'} 
                         width={300}
                         height={300}
                         className='rounded-md shadow-md hover:shadow-lg'
                     />
                 </div> 
-                <div id='contact' className='flex flex-col text-slategray self-start py-20'> 
+                <div id='contact' className='flex flex-col text-default self-start py-20'> 
                     <h3 className='text-2xl font-bold mb-10'>Want to contact us?</h3>
                     <p>
                         Have additional questions? Reach out directly
@@ -75,7 +65,7 @@ const About: React.FC = () => {
 
             <Footer />
         </Layout>
-    )
-}
+    );
+};
 
 export default About;
